333dbb9bdea0315f7e42a6d20f0b5859cc894857
vipul02 <vipul.2.july@gmail.com>
2019-11-16 03:29:40 +0530

    error resolved and feature added in main.py

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template, request
 from flask_wtf.csrf import CSRFProtect
+import os
 from predict import predict_class
 
+SECRET_KEY = os.urandom(32)
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = SECRET_KEY
 csrf = CSRFProtect(app)
 
 @app.route('/', methods=['POST', 'GET'])
-def home(request):
+def home():
 	error = None
 	if request.method == 'POST':
 		img = request.POST.get('canvasData')
