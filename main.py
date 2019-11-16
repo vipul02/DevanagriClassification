fa9e142d0e2b7fdb611c022b5001b73815ecb11b
vipul02 <vipul.2.july@gmail.com>
2019-11-16 15:07:33 +0530

    error resolved canvas

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, request
-from flask_wtf.csrf import CSRFProtect
+# from flask_wtf.csrf import CSRFProtect
 import os
 from predict import predict_class
 
-SECRET_KEY = os.urandom(32)
+# SECRET_KEY = os.urandom(32)
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = SECRET_KEY
-csrf = CSRFProtect(app)
+# app.config['SECRET_KEY'] = SECRET_KEY
+# csrf = CSRFProtect(app)
 
 @app.route('/', methods=['POST', 'GET'])
 def home():
